c5d32db3bf03c6ce12fe6579f404a5f3a5555f01
luuk-royal <luukkaalverink@gmail.com>
2024-06-16 20:34:42 +0200

    Made events page

---
+++ tapawingo_hike_admin_panel/src/app/(dashboard)/organisations/[organisationId]/events/editOrCreateEventDialog.tsx
@@ -0,0 +1,79 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import Button from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Organisation } from '@/types/organisation';
+import { FaEdit } from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+
+type EditOrCreateDialogProps = {
+  value?: Organisation;
+  onSave: (data: Organisation, isEdit: boolean) => void;
+};
+
+export function EditOrCreateDialog({ value, onSave }: EditOrCreateDialogProps) {
+  const isEdit = !!value;
+  const [name, setName] = useState(value?.name || "");
+
+  useEffect(() => {
+    setName(value?.name || "");
+  }, [value]);
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const updatedValue: Organisation = {
+      id: value?.id,
+      name,
+    };
+    onSave(updatedValue, isEdit);
+  };
+
+  return (
+      <Dialog>
+        <DialogTrigger asChild>
+          {isEdit ? (
+              <button><FaEdit /></button>
+          ) : (
+              <Button type="button">Create Organisation</Button>
+          )}
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>{isEdit ? "Edit event" : "Create event"}</DialogTitle>
+            <DialogDescription>
+              {isEdit
+                  ? "Make changes to your event here. Click save when you're done."
+                  : "Fill in the details below to create a new event."}
+            </DialogDescription>
+          </DialogHeader>
+          <form onSubmit={handleSubmit}>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="col-span-3"
+                    required
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">{isEdit ? "Save changes" : "Create organisation"}</Button>
+            </DialogFooter>
+          </form>
+        </DialogContent>
+      </Dialog>
+  );
+}
